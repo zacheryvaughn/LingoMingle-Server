6fb161ce4f87de943ddcf3f10b24d9f9925c8034
Zac Vaughn <zacwilliamvaughn@gmail.com>
2024-04-11 21:04:53 +0800

    cleaned up events, and improved handling connect/disconnect

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ server.listen(PORT, () => {
 const io = new Server(server, {
   cors: {
     origin: ["http://127.0.0.1:5500", "https://lingomingle.com"],
-    // origin: ["http://127.0.0.1:5500"],
     methods: ["GET", "POST"],
     credentials: true,
   },
@@ -27,64 +26,118 @@ app.get('/', (req, res) => {
 const waitingUsers = new Set();
 const userPairs = new Map();
 const privateRooms = new Map();
+const onlineUsers = new Set(); // Track online users with a set
 
-function cleanUp() {
-  const connectedSocketIds = new Set(Array.from(io.sockets.sockets.keys()));
-  let removedWaitingUsers = 0;
-  let removedPairs = 0;
-  let removedRooms = 0;
-
-  // Clean up waitingUsers
-  waitingUsers.forEach((userId) => {
-    if (!connectedSocketIds.has(userId)) {
-      waitingUsers.delete(userId);
-      removedWaitingUsers++;
-    }
-  });
-
-  // Clean up userPairs
-  const toRemoveFromUserPairs = [];
-  userPairs.forEach((partnerId, userId) => {
-    if (!connectedSocketIds.has(userId) || !connectedSocketIds.has(partnerId)) {
-      toRemoveFromUserPairs.push(userId, partnerId);
-    }
-  });
-
-  toRemoveFromUserPairs.forEach((userId) => {
-    if (userPairs.delete(userId)) {
-      removedPairs++;
-    }
-  });
-
-  // Clean up privateRooms
-  privateRooms.forEach((users, roomId) => {
-    const connectedUsers = users.filter((userId) =>
-      connectedSocketIds.has(userId)
-    );
-    if (connectedUsers.length === 0) {
-      privateRooms.delete(roomId);
-      removedRooms++;
-    } else if (connectedUsers.length < users.length) {
-      privateRooms.set(roomId, connectedUsers);
-    }
-  });
-
-  // Log cleanup details
-  if (removedWaitingUsers > 0 || removedPairs > 0 || removedRooms > 0) {
-    console.log(
-      `Cleanup completed: ${removedWaitingUsers} waitingUsers removed, ${removedPairs / 2
-      } pairs removed, ${removedRooms} empty rooms removed.`
-    );
-  }
+function updateOnlineUsersCount() {
+  io.emit("onlineUsers", onlineUsers.size);
 }
-setInterval(cleanUp, 10000);
+
+io.on("connection", (socket) => {
+  onlineUsers.add(socket.id); // Add new connection to online users tracking
+  updateOnlineUsersCount();
+
+  socket.on("disconnect", () => {
+    onlineUsers.delete(socket.id); // Remove connection from online users tracking
+    updateOnlineUsersCount();
+    handleDisconnection(socket.id);
+  });
+
+  socket.on("pairRequest", () => {
+    waitingUsers.add(socket.id);
+    socket.emit("waiting");
+    pairUsers();
+  });
+
+  socket.on("unpairRequest", () => {
+    unpairUsers(socket.id);
+  });
+
+  socket.on("unwaitRequest", () => {
+    waitingUsers.delete(socket.id);
+    socket.emit("unpaired");
+  });
+
+  socket.on("skipRequest", () => {
+    skipUsers(socket.id);
+  });
+
+  socket.on("createPrivateRoom", (roomId) => {
+    if (!privateRooms.has(roomId) && roomId.length > 6) {
+      privateRooms.set(roomId, [socket.id]);
+      socket.join(roomId);
+      socket.emit("privateRoomCreated", { roomId });
+    } else {
+      socket.emit("roomError", "Room already exists or invalid ID");
+    }
+  });
+
+  socket.on("joinPrivateRoom", (roomId) => {
+    if (privateRooms.has(roomId)) {
+      const roomParticipants = privateRooms.get(roomId);
+      if (roomParticipants.length < 2) {
+        roomParticipants.push(socket.id);
+        socket.join(roomId);
+        if (roomParticipants.length === 2) {
+          const [user1Id, user2Id] = roomParticipants;
+          io.to(roomId).emit("pairedInPrivateRoom", { roomId, user1Id, user2Id });
+          io.to(user1Id).emit("initiateCall");
+          console.log(`Both users are now in room: ${roomId}`);
+        } else {
+          socket.emit("waitingForPartner", { roomId });
+        }
+      } else {
+        socket.emit("roomError", "Room is already full");
+      }
+    } else {
+      socket.emit("roomError", "Room does not exist");
+    }
+  });
+
+  socket.on("leavePrivateRoom", () => {
+    for (let [roomId, users] of privateRooms.entries()) {
+      if (users.includes(socket.id)) {
+        users.forEach((userId) => {
+          io.to(userId).emit("unpaired");
+          io.sockets.sockets.get(userId).leave(roomId);
+        });
+        privateRooms.delete(roomId); // Delete the room once empty
+        break;
+      }
+    }
+  });
+
+  socket.on("messageFromClient", (message) => {
+    sendMessageToPartner(socket.id, message);
+  });
+
+  socket.on('sendOffer', data => {
+    console.log(`Relaying offer from ${socket.id} to ${data.to}`);
+    socket.to(data.to).emit('receiveOffer', { offer: data.offer, from: socket.id });
+  });
+
+  socket.on('sendAnswer', data => {
+    console.log(`Relaying answer from ${socket.id} to ${data.to}`);
+    socket.to(data.to).emit('receiveAnswer', { answer: data.answer });
+  });
+
+  socket.on('sendCandidate', data => {
+    console.log(`Relaying ICE candidate from ${socket.id} to ${data.to}`);
+    socket.to(data.to).emit('receiveCandidate', { candidate: data.candidate });
+  });
+});
+
+function handleDisconnection(socketId) {
+  waitingUsers.delete(socketId);
+  if (userPairs.has(socketId)) {
+    unpairUsers(socketId);
+  }
+  console.log("--Disconnected:", socketId);
+}
 
 function pairUsers() {
   function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
-      // Pick a remaining element
       const j = Math.floor(Math.random() * (i + 1));
-      // And swap it with the current element
       [array[i], array[j]] = [array[j], array[i]];
     }
   }
@@ -121,23 +174,20 @@ function pairUsers() {
 }
 
 function unpairUsers(socketId) {
-  if (userPairs.has(socketId)) {
-    const partnerId = userPairs.get(socketId);
+  const partnerId = userPairs.get(socketId);
+  if (partnerId) {
     const roomName = `${socketId}_${partnerId}`;
-
-    [socketId, partnerId].forEach((id) => {
+    [socketId, partnerId].forEach(id => {
       const socket = io.sockets.sockets.get(id);
       if (socket) {
         socket.leave(roomName);
-        socket.emit(id === socketId ? "unpaired" : "waiting");
-        if (id === partnerId) waitingUsers.add(id);
+        socket.emit("unpaired");
+        waitingUsers.add(id);
       }
     });
 
     userPairs.delete(socketId);
     userPairs.delete(partnerId);
-
-    pairUsers();
   }
 }
 
@@ -145,32 +195,22 @@ function skipUsers(socketId) {
   if (userPairs.has(socketId)) {
     const partnerId = userPairs.get(socketId);
     const roomName = `${socketId}_${partnerId}`;
-
-    // Prepare both users for re-pairing without emitting 'unpaired'
-    [socketId, partnerId].forEach((userId) => {
-      const userSocket = io.sockets.sockets.get(userId);
-      if (userSocket) {
-        // Ensure users leave their current room to avoid message crossover
-        userSocket.leave(roomName);
-        // Re-add both users to waitingUsers for immediate re-pairing
-        waitingUsers.add(userId);
-        // Notify both users they're being re-paired
-        userSocket.emit("waiting");
+    [socketId, partnerId].forEach(id => {
+      const socket = io.sockets.sockets.get(id);
+      if (socket) {
+        socket.leave(roomName);
+        waitingUsers.add(id);
+        socket.emit("waiting");
       }
     });
 
-    // Remove the users from the userPairs map
     userPairs.delete(socketId);
     userPairs.delete(partnerId);
-
-    // Attempt to re-pair users including the ones just skipped
     pairUsers();
   }
 }
 
-// Function that determined how messages should be routed
 function sendMessageToPartner(senderId, message) {
-  // Check if the sender is in a private room
   let isInPrivateRoom = false;
   let roomIdToSend = null;
   for (let [roomId, users] of privateRooms.entries()) {
@@ -182,130 +222,9 @@ function sendMessageToPartner(senderId, message) {
   }
 
   if (isInPrivateRoom && roomIdToSend) {
-    // If the sender is in a private room, emit the message to the other user in the room
-    io.to(roomIdToSend).emit("messageFromServer", {
-      sender: senderId,
-      text: message,
-    });
+    io.to(roomIdToSend).emit("messageFromServer", { sender: senderId, text: message });
   } else if (userPairs.has(senderId)) {
-    // If the sender is randomly paired, emit the message to the paired user
     const partnerId = userPairs.get(senderId);
-    io.to(partnerId).emit("messageFromServer", {
-      sender: senderId,
-      text: message,
-    });
+    io.to(partnerId).emit("messageFromServer", { sender: senderId, text: message });
   }
 }
-
-io.on("connection", (socket) => {
-  socket.emit("yourId", socket.id);
-  console.log("--Connected:", socket.id);
-  io.emit("onlineUsers", io.engine.clientsCount);
-
-  socket.on("disconnect", () => {
-    console.log("--Disconnected:", socket.id);
-    io.emit("onlineUsers", io.engine.clientsCount);
-    waitingUsers.delete(socket.id);
-    if (userPairs.has(socket.id)) {
-      unpairUsers(socket.id);
-    }
-  });
-
-  socket.on("pairRequest", () => {
-    waitingUsers.add(socket.id);
-    socket.emit("waiting");
-    pairUsers();
-  });
-
-  socket.on("unpairRequest", () => {
-    unpairUsers(socket.id);
-  });
-
-  socket.on("unwaitRequest", () => {
-    waitingUsers.delete(socket.id);
-    socket.emit("unpaired");
-  });
-
-  socket.on("skipRequest", () => {
-    skipUsers(socket.id);
-  });
-
-  socket.on("createPrivateRoom", (roomId) => {
-    // Ensure room doesn't already exist and roomId is valid
-    if (!privateRooms.has(roomId) && roomId.length > 6) {
-      privateRooms.set(roomId, [socket.id]);
-      socket.join(roomId);
-      socket.emit("privateRoomCreated", { roomId });
-    } else {
-      socket.emit("roomError", "Room already exists or invalid ID");
-    }
-  });
-
-  socket.on("joinPrivateRoom", (roomId) => {
-    if (privateRooms.has(roomId)) {
-      const roomParticipants = privateRooms.get(roomId);
-
-      if (roomParticipants.length < 2) {
-        roomParticipants.push(socket.id);
-        socket.join(roomId);
-
-        // If the room now has two participants
-        if (roomParticipants.length === 2) {
-          const [user1Id, user2Id] = roomParticipants; // Assuming the first element is the existing user
-
-          // Emit to the room, all participants get the same message
-          io.to(roomId).emit("pairedInPrivateRoom", { roomId, user1Id, user2Id });
-          io.to(user1Id).emit("initiateCall");
-
-          console.log(`Both users are now in room: ${roomId}`);
-        } else {
-          // Notify the single user that they are waiting for a partner
-          socket.emit("waitingForPartner", { roomId });
-        }
-      } else {
-        socket.emit("roomError", "Room is already full");
-      }
-    } else {
-      socket.emit("roomError", "Room does not exist");
-    }
-  });
-
-
-  socket.on("leavePrivateRoom", () => {
-    // Find and leave the private room
-    for (let [roomId, users] of privateRooms.entries()) {
-      if (users.includes(socket.id)) {
-        users.forEach((userId) => {
-          io.to(userId).emit("unpaired");
-          io.sockets.sockets.get(userId).leave(roomId);
-        });
-        privateRooms.delete(roomId); // Delete the room once empty
-        break;
-      }
-    }
-  });
-
-  // Run message emit function when message is received from client.
-  socket.on("messageFromClient", (message) => {
-    sendMessageToPartner(socket.id, message);
-  });
-
-  // Relaying the WebRTC offer
-  socket.on('sendOffer', data => {
-    console.log(`Relaying offer from ${socket.id} to ${data.to}`);
-    socket.to(data.to).emit('receiveOffer', { offer: data.offer, from: socket.id });
-  });
-
-  // Relaying the WebRTC answer
-  socket.on('sendAnswer', data => {
-    console.log(`Relaying answer from ${socket.id} to ${data.to}`);
-    socket.to(data.to).emit('receiveAnswer', { answer: data.answer });
-  });
-
-  // Relaying ICE candidates
-  socket.on('sendCandidate', data => {
-    console.log(`Relaying ICE candidate from ${socket.id} to ${data.to}`);
-    socket.to(data.to).emit('receiveCandidate', { candidate: data.candidate });
-  });
-
-});
